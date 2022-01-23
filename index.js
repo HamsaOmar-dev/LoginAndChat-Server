30fdbc7e138e9b2617eefa28feb425f593397d5a
Hamsa Omar <hamsaomar@hamsas-air.home>
2022-01-23 10:31:21 -0600

    Add Start Script

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-dotenv.config();
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
